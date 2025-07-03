ec64b5cfc8a5b501563120a95b3bbbed71a3b251
caeltarifa <caeltarifa@gmail.com>
2025-07-02 23:46:11 -0300

    initial - Needs rather sorting out

---
+++ inventory.py
@@ -0,0 +1,28 @@
+from pyinfra import host
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SSH_PRIVATE_KEY = "~/.ssh/ubuntuKey.pem"
+_PASSWD=os.getenv("SERVER_PASSWD")
+# tcp://0.tcp.sa.ngrok.io:16891                             
+
+hosts = [
+    ("0.tcp.sa.ngrok.io", {"ssh_user": "admin_sumato", "ssh_port": 16891, "ssh_password":_PASSWD}),
+    #("20.197.227.116", {"ssh_user": "SumatoAdmin","ssh_key": SSH_PRIVATE_KEY}),
+]
+
+inventory = (
+    hosts
+)
+
+#hosts = ["20.197.227.116"]
+#inventory = (
+#    hosts,
+#    {
+#        "ssh_user": "SumatoAdmin",
+#        "ssh_key": SSH_PRIVATE_KEY,
+#    },
+#)
+
