b9898c685df6a382d4c3bf8fd6257e04c06e056a
caeltarifa <caeltarifa@gmail.com>
2025-07-17 15:04:15 -0300

    Fetch and copy azure data to their folders

--- Tasks/fetch_azure_data.py
+++ Tasks/fetch_azure_data.py
@@ -1,6 +1,6 @@
 from pyinfra.operations import files, apt, server
 
-files.get(
+files.download(
     name="Download Microsoft package repo config",
     src="https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb",
     dest="/tmp/packages-microsoft-prod.deb",
@@ -9,32 +9,51 @@ files.get(
 server.shell(
     name="Install Microsoft package repo config",
     commands=["dpkg -i /tmp/packages-microsoft-prod.deb"],
-    sudo=True,
+    _sudo=True,
+    _sudo_password="123",
 )
 
 apt.update(
     name="Update apt package list",
-    sudo=True,
+    _sudo=True,
 )
 
 apt.packages(
     name="Install azcopy",
     packages=["azcopy"],
-    sudo=True,
+    _sudo=True,
 )
 
 # fetche files and sensitive credentials from azure's container
 
-source_url = YOUR_SHARED_ACCESS_SIGNATURE
+source_url= "YOUR_SHARED_ACCESS_SIGNATURE"
+AZURE_FILES_DIR="azure_files"
 
-server.shell(
+download_result = server.shell(
     name="Download assets with AzCopy",
     commands=[
-        f'azcopy copy --recursive "{source_url}" "assets"',
-    ],
-    _shell_args=dict(
-        shell="bash", 
-    ),
+        f'azcopy copy --recursive "{source_url}" "{AZURE_FILES_DIR}"',
+    ]
 )
 
+# Carry on files from one to another place
+if download_result.changed:
+    SOURCE = f"{AZURE_FILES_DIR}/deploy-assets/network_config.csv"
+    DESTINATION = "Tasks/master-image-v2/network_as_service"
 
+    files.directory(path=DESTINATION, present=True)
+    server.shell(
+        name="Copying network listing file",
+        commands=[f"cp {SOURCE} {DESTINATION}"]
+    )
+
+    SOURCE = f"{AZURE_FILES_DIR}/deploy-assets/.env_connections_strings.csv"
+    DESTINATION = "Tasks/app"
+
+    files.directory(path=DESTINATION, present=True)
+    server.shell(
+        name="Copying IoT Hub strings file",
+        commands=[f"cp {SOURCE} {DESTINATION}"]
+    )
+else:
+    print("No effects on AzCopy downloading")
