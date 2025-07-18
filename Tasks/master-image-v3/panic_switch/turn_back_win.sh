35d3f45010338e88ef83aefbc808d7f40e5c6251
caeltarifa <caeltarifa@gmail.com>
2025-07-18 09:41:47 -0300

    Exchange the names of booloader configs between win and linux for ESP partition

--- Tasks/master-image-v3/panic_switch/turn_back_win.sh
+++ Tasks/master-image-v3/panic_switch/turn_back_win.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#chown root:root this_bash
-
-# Wait for 10 minutes (600 seconds)
+# Wait for 10 minutes
 sleep 600
 
 ESP_MOUNT="/boot/efi/EFI/refind"
 
 if [ -f "$ESP_MOUNT/refind.conf" ]; then
-    mv "$ESP_MOUNT/refind.conf" "$ESP_MOUNT/refind.conf.v2"
+    #Swapping the bootloader configuration files between windows and linux startup. 
+    cp "$ESP_MOUNT/refind.conf" "$ESP_MOUNT/refind_tolnx.conf"
+    mv "$ESP_MOUNT/refind_towin.conf" "$ESP_MOUNT/refind.conf"
 else
     echo "refind.conf not found at $ESP_MOUNT" >&2
     exit 1
