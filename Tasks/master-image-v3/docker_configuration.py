a6d934192f73aed9a4349b587a7c7a6a8449d74d
caeltarifa <caeltarifa@gmail.com>
2025-07-19 03:01:13 -0300

    Remote docker-nvidia configuration

--- Tasks/master-image-v3/docker_configuration.py
+++ Tasks/master-image-v3/docker_configuration.py
@@ -1,5 +1,6 @@
-from pyinfra import host
-from operations import DockerSetup, apt
+from pyinfra import host, server
+from pyinfra.operations import apt
+from Operations.docker_nvidia_setup import DockerSetup
 
 docker_setup = DockerSetup(host)
 
@@ -8,16 +9,23 @@ docker_setup = DockerSetup(host)
 docker_setup.install_necessary_packages()
 docker_setup.add_docker_gpg_key()
 docker_setup.add_docker_repo()
-apt.update()
+apt.packages(
+    name="Package update",
+    latest=True
+)
 docker_setup.install_docker_packages()
-docker_setup.run_docker_hello_world()
+docker_setup.add_user_docker_group()
 
 # NVIDIA driver setup
-docker_setup.install_nvidia_driver()  # default to driver version 535
-docker_setup.reboot_system()
-docker_setup.check_nvidia_smi()
+docker_setup.install_nvidia_driver()  
 
 # NVIDIA Container Toolkit setup
 docker_setup.install_nvidia_container_toolkit()
-docker_setup.reboot_system()  
 
+server.shell(
+    name="A reboot is compulsory",
+    commands=['echo "Reboot is a must"']
+)
+
+#docker_setup.run_docker_hello_world()
+docker_setup.check_nvidia_smi()
