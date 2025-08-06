d56a6ccc844ef6a46b8f42b975053c7d307332e0
caeltarifa <caeltarifa@gmail.com>
2025-08-06 06:06:26 -0300

    Image distribution

---
+++ Inventories/on_production.py
@@ -0,0 +1,10 @@
+hosts = [
+    "admin_sumato@110.34.35.16", #Cond
+    "admin_sumato@110.70.35.252",
+    "admin_sumato@110.66.36.40",
+    "admin_sumato@10.113.134.34",
+    "admin_sumato@10.113.130.236",
+    "admin_sumato@110.79.36.231",
+    "admin_sumato@110.63.35.28",
+    "admin_sumato@110.47.35.28",
+]
